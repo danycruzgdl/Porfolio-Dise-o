b18853076247006c9d5d6694567942bf667c9721
Daniel Cruz <danycruzgdl@gmail.com>
2024-07-21 19:17:38 -0600

    test

---
+++ Branding/branding..js
@@ -0,0 +1,19 @@
+const imgList = document.querySelectorAll(`img`);
+const ligthbox = document.querySelector(`.lightbox`);
+const grande = document.querySelector(`.grande`);
+const closebtn = document.querySelector(`.close`);
+const removeNav = document.querySelector(`.container-navegation`);
+
+
+imgList.forEach(( eachImg , index )=>{
+    imgList[index].addEventListener(`pointerdown`,()=> {
+       ligthbox.classList.add(`isactive`)
+       grande.src = imgList[index].src
+       removeNav.setAttribute('style', 'display:none')
+    })
+ });
+ 
+ closebtn.addEventListener(`pointerdown`,()=>{
+   ligthbox.classList.remove(`isactive`)
+   removeNav.setAttribute('style', 'display:flex')
+ })
---
+++ main.js
@@ -0,0 +1,52 @@
+var branding = document.getElementById('branding');
+branding.addEventListener('click', function() { 
+   location.href = "branding.html";
+}) ;
+
+var demorell = document.getElementById('demorell');
+demorell.addEventListener('click', function() { 
+   location.href = "diseño_ui";
+}) ;
+
+var diseño_ui = document.getElementById('diseño-ui');
+diseño_ui.addEventListener('click', function() { 
+   location.href = "demorell.html";
+}) ;
+
+var impresion_digital = document.getElementById('impresion-digital');
+impresion_digital.addEventListener('click', function() { 
+   location.href = "impresion_digital.html";
+}) ;
+
+var desarrollo_web = document.getElementById('desarrollo-web');
+desarrollo_web.addEventListener('click', function() { 
+   location.href = "desarrollo_web.html";
+}) ;
+
+var illustracion_digital = document.getElementById('illustracion-digital');
+illustracion_digital.addEventListener('click', function() { 
+   location.href = "illustracion_digital.html";
+}) ;
+
+var photomontage = document.getElementById('photomontage');
+photomontage.addEventListener('click', function() { 
+   location.href = "photomontage.html";
+}) ;
+
+/* ============================== LIGHTBOX BRANDING HTML ============================= >>>> */
+// const imgList = document.getElementById("img-branding-uno");
+// const ligthbox = document.querySelector('ligthbox');
+
+// imgList.addEventListener('click', function(){
+//    alert('asdasd');
+// })
+
+
+// imgList.forEach(( eachImg , index )=>{
+//    imgList[index].addEventListener('click',()=> {
+//       ligthbox.classList.add('isactive');
+      
+//    })
+// })
+
+
